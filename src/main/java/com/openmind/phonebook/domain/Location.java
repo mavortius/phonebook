f383f7ae856902c79220ffcc1afc2d84ef1a5fbf
Marcelo Martins <m.a.martins@live.com>
2017-10-17 21:41:07 -0200

    Creation of domain entities

---
+++ src/main/java/com/openmind/phonebook/domain/Location.java
@@ -0,0 +1,117 @@
+package com.openmind.phonebook.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+
+    @Id
+    private String id;
+    private String name;
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+    private String mainPhone;
+    private String mainFax;
+
+    public Location() {
+    }
+
+    public Location(String id, String name, String address1, String address2,
+                    String city, String state, String zip, String country, String mainPhone, String mainFax) {
+        this.id = id;
+        this.name = name;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.country = country;
+        this.mainPhone = mainPhone;
+        this.mainFax = mainFax;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getMainPhone() {
+        return mainPhone;
+    }
+
+    public void setMainPhone(String mainPhone) {
+        this.mainPhone = mainPhone;
+    }
+
+    public String getMainFax() {
+        return mainFax;
+    }
+
+    public void setMainFax(String mainFax) {
+        this.mainFax = mainFax;
+    }
+}
---
+++ src/main/java/com/openmind/phonebook/domain/Person.java
@@ -0,0 +1,118 @@
+package com.openmind.phonebook.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    private String id;
+    private String locationId;
+    private String firstName;
+    private String lastName;
+    private String jobTitle;
+    private String department;
+    private String phoneNumber;
+    private String faxNumber;
+    private String cellNumber;
+    private String managerId;
+
+    public Person() {
+    }
+
+    public Person(String id, String locationId, String firstName, String lastName,
+                  String jobTitle, String department, String phoneNumber, String faxNumber, String cellNumber,
+                  String managerId) {
+        this.id = id;
+        this.locationId = locationId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.jobTitle = jobTitle;
+        this.department = department;
+        this.phoneNumber = phoneNumber;
+        this.faxNumber = faxNumber;
+        this.cellNumber = cellNumber;
+        this.managerId = managerId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(String locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getCellNumber() {
+        return cellNumber;
+    }
+
+    public void setCellNumber(String cellNumber) {
+        this.cellNumber = cellNumber;
+    }
+
+    public String getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(String managerId) {
+        this.managerId = managerId;
+    }
+}
